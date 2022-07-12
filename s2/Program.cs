04fe6a5cfd0fd5c4b9ae2f03ae88abfd1fa8c354
yury <yuriy.t87@gmail.com>
2022-07-12 20:33:18 +0300

    ДЗ по 2 семинару

--- s2/Program.cs
+++ s2/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
-namespace s2
+namespace ex10
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+            Console.Write("Введите трехзначное число: ");
+            var num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Второе число: " + GetSecondNumber(num));
+        }
+
+
+        static int GetSecondNumber(int num)
+        {
+            if (num < 0)
+                num = -num;
+
+            if (num < 99 || num > 1000)
+                return -1;
+
+            num = num / 10;
+
+            return num % 10 ;
         }
     }
 }
